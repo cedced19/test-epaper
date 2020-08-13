0b69d48b670813e9051b43a6177cc0ae6a7fce94
Cédric JUNG <cedced19@gmail.com>
2020-08-13 13:35:07 +0200

    Firt tests

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # test-epaper
 I have brought an epaper displayer I wanted to try it.
+
+## Links
+
+* https://www.youtube.com/watch?v=mySnfyyL4i4
+* https://www.waveshare.com/w/upload/6/6a/4.2inch-e-paper-specification.pdf
+* https://www.waveshare.com/wiki/4.2inch_e-Paper_Module
+
---
+++ example1/PagedDisplayForSmallRam/PagedDisplayForSmallRam.ino
@@ -0,0 +1,62 @@
+// PagedDisplayForSmallRam : test example for e-Paper displays from Waveshare and from Dalian Good Display Inc.
+//                           shows how the full display screen can be used with Adafruit_GFX on small RAM Arduinos
+//                           runs also on Arduinos with more RAM, e.g. ESP8266, for test
+//
+// Created by Jean-Marc Zingg based on demo code from Good Display for GDEP015OC1.
+//
+// The e-paper displays are available from:
+//
+// https://www.aliexpress.com/store/product/Wholesale-1-54inch-E-Ink-display-module-with-embedded-controller-200x200-Communicate-via-SPI-interface-Supports/216233_32824535312.html
+//
+// http://www.buy-lcd.com/index.php?route=product/product&path=2897_8363&product_id=35120
+// or https://www.aliexpress.com/store/product/E001-1-54-inch-partial-refresh-Small-size-dot-matrix-e-paper-display/600281_32815089163.html
+//
+
+// mapping suggestion for AVR, UNO, NANO etc.
+// BUSY -> 7, RST -> 9, DC -> 8, CS-> 10, CLK -> 13, DIN -> 11
+
+#include <GxEPD.h>
+
+#include <GxGDEW042T2/GxGDEW042T2.h>      // 4.2" b/w
+
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <GxIO/GxIO.h>
+
+// FreeFonts from Adafruit_GFX
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+GxIO_Class io(SPI, /*CS=*/ SS, /*DC=*/ 8, /*RST=*/ 9); // arbitrary selection of 8, 9 selected for default of GxEPD_Class
+GxEPD_Class display(io, /*RST=*/ 9, /*BUSY=*/ 7); // default selection of (9), 7
+
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+  display.init(115200); // enable diagnostic output on Serial
+  Serial.println("setup done");
+  delay(5000);
+  display.drawPaged(showFontCallback);
+  
+}
+
+void loop()
+{
+  
+}
+
+void showFontCallback()
+{
+  const char* name = "FreeMonoBold9pt7b";
+  const GFXfont* f = &FreeMonoBold9pt7b;
+  display.fillScreen(GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f);
+  display.setCursor(0, 0);
+  display.println();
+  display.println("Test d'ecran e-Paper");
+  display.println();
+  display.println();
+  display.println("Cedric JUNG");
+}
